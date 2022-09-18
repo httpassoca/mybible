d55125f1e6d99dabbeccbf9c74f464c5b430eb26
httpassoca <me@passoca.dev>
2022-09-18 22:02:00 +0100

    fc

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <div class="container mx-auto mt-4">
+    <header>
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">Sobre</RouterLink>
+      </nav>
+      <form action="get">
+        <input type="text">
+      </form>
+    </header>
+    <main>
+      <RouterView />
+    </main>
+  </div>
+</template>
+
+<style lang="sass" scoped>
+.container
+  max-width: 630px
+</style>
---
+++ src/views/AboutView.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="about">
+    <p>Feito por Rafael Passoca❤️</p>
+  </div>
+</template>
