aca9637a289063132d031730396e8edd8d387cbb
httpassoca <me@passoca.dev>
2022-11-25 00:35:52 +0000

    add bible chapters and verses

--- src/services/bible.ts
+++ src/services/bible.ts
@@ -1,7 +1,13 @@
 import axios from '../http-common';
 
 export default {
-  getBooks() {
+  async getBook(book: string) {
+    return axios.get(`/books/${book}`).then(res => res.data)
+  },
+  async getBooks() {
     return axios.get('/books').then(res => res.data)
+  },
+  async getChapter(book: string, chapter: number = 1) {
+    return axios.get(`/verses/nvi/${book}/${chapter}`).then(res => res.data)
   },
 }
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,30 +1,60 @@
 <template>
-  <div class="flex justify-center">
-    <select v-if="books.length">
-      <option v-for="book in books">{{book?.name}}</option>
+  <div class="home">
+    <select v-if="books.length" v-model="selectedBook" @change="getBook()">
+      <option v-for="bok in books" :value="bok.abbrev.pt">{{ bok.name }}</option>
     </select>
+    <div v-if="state.book" class="bible">
+      <h2>
+        {{ state.book.book.name }}
+      </h2>
+      <p v-for="verse in state.book.verses">
+        <b>{{ verse.number }}</b> {{ verse.text }}
+      </p>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
-  import { onMounted, ref} from 'vue'
-  import bible from '../services/bible'
-  
-  interface IBook {
-    abbrev: {[k: string]: string};
-    author: string;
-    chapters: number;
-    group: string;
-    name: string;
-    testament: string;
-  }
+import { onMounted, ref, reactive } from 'vue'
+import bible from '../services/bible'
 
-  let books = ref<IBook[]>([])
-  
-  onMounted(async () => {
-    books.value = await bible.getBooks();
-  });
+interface IBook {
+  abbrev: { [k: string]: string };
+  author: string;
+  chapters: number;
+  group: string;
+  name: string;
+  testament: string;
+}
+
+let books = ref<IBook[]>([])
+let selectedBook = ref<string>('');
+const state = reactive({
+  book: null as any,
+})
+
+onMounted(async () => {
+  books.value = await bible.getBooks();
+});
+
+async function getBook() {
+  state.book = await bible.getChapter(selectedBook.value);
+}
 </script>
+
 <style lang="sass">
+select
+  color: black
+.bible
+  @apply mt-4
+  > h2
+    text-align: center
+    font-size: 2rem
+  p
+    margin-top: 1rem
+    font-size: 1.25rem
+    font-style: italic
+    b
+      margin-right: 1rem
 
 </style>
