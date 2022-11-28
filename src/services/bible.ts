7fd8002015b54510848cea31c5e59e5cd6467d1c
httpassoca <me@passoca.dev>
2022-11-28 04:11:39 +0000

    update bible service

--- src/services/bible.ts
+++ src/services/bible.ts
@@ -1,13 +1,13 @@
-import axios from '../http-common';
+import axios from "../http-common";
 
 export default {
   async getBook(book: string) {
-    return axios.get(`/books/${book}`).then(res => res.data)
+    return axios.get(`/books/${book}`).then((res) => res.data);
   },
   async getBooks() {
-    return axios.get('/books').then(res => res.data)
+    return axios.get("/books").then((res) => res.data);
   },
-  async getChapter(book: string, chapter: number = 1) {
-    return axios.get(`/verses/nvi/${book}/${chapter}`).then(res => res.data)
+  async getVerses(book: string, chapter: number) {
+    return axios.get(`/verses/nvi/${book}/${chapter}`).then((res) => res.data);
   },
-}
+};
