831ff77d8739986c6e2850154538899e907a093b
Rafael Freitas <49541181+httpassoca@users.noreply.github.com>
2022-12-15 09:05:02 +0000

    fix security problem

--- src/http-common.ts
+++ src/http-common.ts
@@ -4,6 +4,6 @@ export default axios.create({
   baseURL: "https://www.abibliadigital.com.br/api",
   headers: {
     "Content-type": "application/json",
-    "Authorization": "bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IkZyaSBOb3YgMjUgMjAyMiAwMDoxNDo1NiBHTVQrMDAwMC5wcml2QGdtYWlsLmNvbSIsImlhdCI6MTY2OTMzNTI5Nn0.cvLZSFCa4B1zizYBjGNcB5tFogSFbqRlbjk7-DDYrSQ"
+    "Authorization": "bearer AUTH-CODE"
   }
 });
